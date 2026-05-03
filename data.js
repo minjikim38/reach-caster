fdf8ce4b00866f2d9293a1059d94e69174403996
minjikim38 <83162789+minjikim38@users.noreply.github.com>
2026-05-03 21:06:25 +0900

    Update data.js

--- data.js
+++ data.js
@@ -74,8 +74,8 @@ const INTERVIEW_DATA = {
       {
         name: "Reach Predictor",
         resources: [
-          { label: "파일명 또는 링크 이름", url: "https://..." },
-          { label: "(기존) MTS - Reach Caster(GRPs)", url: "https://..." }
+          { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" },
+          { label: "(기존) MTS - Reach Caster(GRPs)", url: "https://ts.mezzomedia.co.kr/predict/grps/detail/2622?pageNum=1&pageSize=50&scenarioType=grps&masterCmpYn&oldScenarioFlag=0&searchYear=2026&searchMonthStart=1&searchMonthEnd=12&searchTeamCode&searchMediaCode&searchCateCode&searchKpi&searchTargetCode&searchBudgetMin&searchBudgetMax&searchStatus&searchType=0&searchText=%EA%B5%AC%EC%A7%80%EC%88%99" }
         ]
       },
       {
@@ -88,28 +88,97 @@ const INTERVIEW_DATA = {
   },
 
   "양은미":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
-    { label: "파일명 또는 링크 이름", url: "https://..." },
-    { label: "(기존) MTS - Reach Caster(GRPs)", url: "https://..." }
+    { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" },
+    { label: "(기존) MTS - Reach Caster(GRPs)", url: "https://ts.mezzomedia.co.kr/predict/grps/detail/3457?pageNum=1&pageSize=50&scenarioType=grps&masterCmpYn&oldScenarioFlag=0&searchYear=2026&searchMonthStart=1&searchMonthEnd=12&searchTeamCode&searchMediaCode&searchCateCode&searchKpi&searchTargetCode&searchBudgetMin&searchBudgetMax&searchStatus&searchType=0&searchText=%EC%96%91%EC%9D%80%EB%AF%B8" }
   ] }, { name: "Ratio Finder", resources: [
-    { label: "파일명 또는 링크 이름", url: "https://..." },
-    { label: "Ratio Finder 사이트", url: "https://..." }
+    { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" },
   ] } ] },
-  "구연수":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "조아라":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "백현기":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "조상진":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "박혜민":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "최선태":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "김소중":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "임혜정":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "한선재":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "김다슬":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "윤경현":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "임지원":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "임찬":    { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "남유리":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "안성욱":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "황나경":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
-  "신지원":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [] }, { name: "Ratio Finder", resources: [] } ] },
+  "구연수":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" },
+    { label: "Ratio Finder 사이트", url: "https://..." }
+  ] }, { name: "Ratio Finder", resources: [
+    { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" }
+  ] } ] },
+  "조아라":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" },
+    { label: "Ratio Finder 사이트", url: "https://..." }
+  ] }, { name: "Ratio Finder", resources: [
+    { label: "파일명.xlsx", url: "https://example.com/file2.xlsx" }
+  ] } ] },
+  "백현기":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "조상진":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "박혜민":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "최선태":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "김소중":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "임혜정":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "한선재":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "김다슬":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "윤경현":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "임지원":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "임찬":    { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "남유리":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "안성욱":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "황나경":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
+  "신지원":  { surveyUrl: DEFAULT_SURVEY_URL, folders: [ { name: "Reach Predictor", resources: [
+    
+  ] }, { name: "Ratio Finder", resources: [
+    
+  ] } ] },
 
 };
